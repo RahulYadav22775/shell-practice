9ef166eb95dd33a5a0db81f2bc9f856f00caf914
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-26 18:34:32 +0530

    changes made

--- script6.sh
+++ script6.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
  
- NUMBER=$1
+ USERID=$(id -u)
 
- if [ $NUMBER -gt 20 ] 
-    then
-    echo " print $NUMBER is gtreater than 20 "
-    else
-    echo "print $NUMBER  is less than 20"   
-fi
+ R=$"\e[31m"
+ G=$"\e[32m"
+ N=$"\e[0m"
+
+
+ if [ $USERID -ne 0 ]
+ then 
+ echo " $R you are not a root user to execute the script $N "
+ exit 1
+ fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo " mysql is not installed "
+else
+    echo " $G mysql is already installed $N "
+    exit 1
+fi    
+
+dnf install mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysql istallation is not success "
+    exit 1
+else
+    echo " $G mysql is successfully installed $N "
+fi    
