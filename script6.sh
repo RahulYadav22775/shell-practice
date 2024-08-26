0a2a2a66c4d57f7e3cd61aa8210d883f903f2dc2
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-26 19:16:45 +0530

    changes made

--- script6.sh
+++ script6.sh
@@ -7,6 +7,15 @@
  N=$"\e[0m"
  B=$"\e[33m"
 
+ VALIDATE () {
+if [ $1 -ne 0 ]
+then
+    echo -e " $R $2 installation is failure $N "
+else 
+    echo -e " $G $2 installation is success $N "
+fi
+ }
+
 
  if [ $USERID -ne 0 ]
  then 
@@ -26,13 +35,15 @@ fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
-then
-    echo "mysql istallation is not success "
+VALIDATE $? "mysql"
+
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql istallation is not success "
     
-else
-    echo  -e " $G mysql is successfully installed $N "
-fi    
+# else
+#     echo  -e " $G mysql is successfully installed $N "
+# fi    
 
 dnf list installed git 
 
@@ -41,14 +52,17 @@ then
     echo " git is not installed "
 else 
     echo  -e " $G git is already installed $N "
-exit 1
+
 fi
 
 dnf install git -y
+VALIDATE $? "git"
 
-if [ $? -ne 0 ]
-then
-    echo -e " $R git installation is failure $N "
-else 
-    echo -e " $G git installation is success $N "
-fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo -e " $R git installation is failure $N "
+# else 
+#     echo -e " $G git installation is success $N "
+# fi
