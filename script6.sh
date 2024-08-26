21990ed298c57e0dc83572012a43d287bff52108
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-26 18:08:06 +0530

    changes made

--- script6.sh
+++ script6.sh
@@ -5,3 +5,17 @@
 
  SUM=$(($NUMBER1+$NUMBER2))
  echo " sum of  $NUMBER1 & $NUMBER2 is : $SUM "
+
+ echo " all the arguments/inputs : $@ "
+ 
+ echo " total number of arguments : $# "
+
+ echo " name of the script : $0 "
+
+ echo " process id of the current script : $$ "
+
+ echo " process id of the previous script : $! "
+
+ echo " present workig directory : $PWD "
+
+ echo " present user home directory : $HOME "
