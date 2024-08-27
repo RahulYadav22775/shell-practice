d0debe3dc3e33127dfed512f47143f922baa858e
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-27 10:40:19 +0530

    installation

--- script6.sh
+++ script6.sh
@@ -7,11 +7,33 @@ G=$"\e[32m"
 Y=$"\e[33m"
 N=$"\e[0m"
 
-if [ $USERID -ne 0 ]
+VALIDATE() {
+
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " $R $2 is not installed $N " 
+    else 
+        echo -e " $G $2 is installed $N "
+    fi
+
+}
+
+CHECK-ROOT(){
+
+    if [ $USERID -ne 0 ]
 then 
     echo  -e " $R you are not root user to execute this script $N "
     exit 1
 fi
+}
+
+# if [ $USERID -ne 0 ]
+# then 
+#     echo  -e " $R you are not root user to execute this script $N "
+#     exit 1
+# fi
+
+CHECK-ROOT()
 
 dnf list installed mysql
 
@@ -21,15 +43,10 @@ then
 
     dnf install mysql -y
 
-    if [ $? -ne 0 ]
-    then 
-        echo -e " $R mysql is not installed $N " 
-    else 
-        echo -e " $G mysql is installed $N "
-    fi
+    VALIDATE() $? "mysql"
 else 
     echo -e " $G mysql is installed $N "
-    exit 1
+    
 fi
 
 # dnf install mysql -y
@@ -41,5 +58,18 @@ fi
 #     echo -e " $G mysql is installed $N "
 # fi
 
+CHECK-ROOT()
 
- 
+ dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+    echo  -e " $Y nginx is not installed ..installing $N "
+
+    dnf install mysql -y
+
+    VALIDATE() $? "nginx"
+else 
+    echo -e " $G nginx is installed $N "
+    exit 1
+fi
