a984da5ace3e8df2e644c19cf4573851d4502a11
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-27 16:23:26 +0530

    installing packages

--- installation.sh
+++ installation.sh
@@ -65,7 +65,7 @@ systemctl status mysqld &>>$LOG_FILE
 VALIDATE $? "status of  mysql-server"
 
 mysql -h 172.31.88.192 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then 
     echo " $Y password is not set up $N " | tee -a >>$LOG_FILE
     mysql_secure_installation --set--root-pass ExpenseApp@1 &>>$LOG_FILE
