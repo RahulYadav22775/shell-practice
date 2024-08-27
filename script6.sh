ed18a1c5bf890c2d28eb0bb95b054a1504f65775
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-27 09:58:39 +0530

    installing check

--- script6.sh
+++ script6.sh
@@ -1,68 +1,36 @@
 #!/bin/bash
- 
- USERID=$(id -u)
 
- R=$"\e[31m"
- G=$"\e[32m"
- N=$"\e[0m"
- B=$"\e[33m"
+USERID=$(id -u)
 
- VALIDATE () {
-if [ $1 -ne 0 ]
-then
-    echo -e " $R $2 installation is failure $N "
-else 
-    echo -e " $G $2 installation is success $N "
+R=$"\e[31m"
+G=$"\e[32m"
+Y=$"\e[33m"
+N=$"\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo " $R you are not root user to execute this script $N "
+    exit 1
 fi
- }
-
-
- if [ $USERID -ne 0 ]
- then 
- echo -e " $R you are not a root user to execute the script $N "
- exit 1
- fi
 
 dnf list installed mysql
 
 if [ $? -ne 0 ]
-then
-    echo " mysql is not installed "
-else
-    echo  -e " $B mysql is already installed $N "
-      
-fi    
+then 
+    echo " $Y mysql is not installed $N "
+else 
+    echo " $G mysql is installed $N "
+    exit 1
+fi
 
 dnf install mysql -y
 
-VALIDATE $? "mysql"
-
-# if [ $? -ne 0 ]
-# then
-#     echo "mysql istallation is not success "
-    
-# else
-#     echo  -e " $G mysql is successfully installed $N "
-# fi    
-
-dnf list installed git 
-
-if [ $? -ne 0 ] 
-then
-    echo " git is not installed "
+if [ $? -ne 0 ]
+then 
+    echo " $R mysql is not installed $N " 
 else 
-    echo  -e " $G git is already installed $N "
-
+    echo " $G mysql is installed $N "
 fi
 
-dnf install git -y
-VALIDATE $? "git"
 
-
-
-# if [ $? -ne 0 ]
-# then
-#     echo -e " $R git installation is failure $N "
-# else 
-#     echo -e " $G git installation is success $N "
-# fi
+ 
