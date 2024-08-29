61ee0dbd9470355f7386747dc8e0f65b718c58ed
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-29 10:13:39 +0530

    auto exit

--- validation.sh
+++ validation.sh
@@ -2,13 +2,14 @@
 
 set -ex
 
-failure() {
-    echo " failed at : $1 :: $2 "
-}
-
-trap 'failure ${LINED} ${BASH_COMMAND}' ERR
 
 echo "hello"
 echo "hi"
 echh "hw r u"
 echo "i am gud "
+
+failure() {
+    echo " failed at : $1 :: $2 "
+}
+
+trap 'failure ${LINED} ${BASH_COMMAND}' ERR
