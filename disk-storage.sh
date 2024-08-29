bf1094534166eecb32a6be16710a38c2958a89fa
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-29 16:41:12 +0530

    auto exit

--- disk-storage.sh
+++ disk-storage.sh
@@ -6,10 +6,10 @@ THRESHOLD=5
 
 while IFS= read -r files
 do
-  FILE_NAME=$files | awk -F " " '{print $NF}'
-  FILE_THRESHOLD=$files | awk -F " " '{print $6F}' | cut -d "%" -f1
-  if [ $FILE_THRESHOLD -gt $THRESHOLD ]
+  FILE_NAME=$(echo $files | awk -F " " '{print $NF}')
+  FILE_THRESHOLD=$(echo $files | awk -F " " '{print $6F}' | cut -d "%" -f1)
+  if [ $FILE_THRESHOLD -ge $THRESHOLD ]
   then 
-      echo " $FILE_NAME is exceeded the threshold memory "
+      echo " $FILE_NAME is exceeded the threshold memory ...current memory is $FILE_THRESHOLD "
   fi
 done <<< $SOURCE_FILES
