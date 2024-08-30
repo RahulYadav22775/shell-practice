17917d09789240f0b0c101394d0012cb1539db27
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-30 20:01:37 +0530

    counting of words

--- words.sh
+++ words.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# USAGE() {
-#      echo " you are not a root user to execute this script "
-#      exit 1
-# }
-
-# if [ $# -eq 0 ]
-# then 
-#     USAGE
-# fi
 
 echo " enter the file name : "
 
 read FILE
 
-if [ -f $FILE ]
+if [-f $FILE ]
 then 
     echo "$FILE exists you can continue " 
 else 
