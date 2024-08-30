d745b458fb63d4a6af5b1ef3e3a89c406d955230
RahulYadav22775 <gopichand556677@gmail.com>
2024-08-30 19:44:48 +0530

    counting of words

--- words.sh
+++ words.sh
@@ -18,10 +18,18 @@ echo " enter the word you want to count "
 
 read WORD
 
+# if [ -f $FILE ]
+# then 
+#     echo "word  in the file "
+#     grep $WORD $FILE
+# else 
+#     echo " file does not exist "
+#     exit 1
+# fi
+
 if [ -f $FILE ]
 then 
-    echo "word  in the file "
-    grep $WORD $FILE
+    uniq -c $FILE
 else 
     echo " file does not exist "
     exit 1
