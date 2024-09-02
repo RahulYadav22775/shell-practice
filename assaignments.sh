0de946f9dbae48bae48d666e9b3b564f72e00c8a
RahulYadav22775 <gopichand556677@gmail.com>
2024-09-02 09:47:04 +0530

    assaignments

---
+++ assaignments.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+FILE_NAME=$1
+
+USAGE() {
+    echo " USAGE:: $0 <FILE-NAME>"
+    exit 1
+}
+
+if [ $@ -eq 0 ]
+then 
+    USAGE
+fi
+
+COUNT=$(cat $FILE_NAME | wc -w)
+
+for i in {1..$COUNT}
+do
+  word=$(cat $FILE_NAME | cut -d " " -f$i)
+  WORD_COUNT=$(grep -o $word $FILE_NAME | wc -l)
+
+  if [ $WORD_COUNT -nq 0 ]
+  then
+      echo "the count of the word $word in the file $FILE_NAME is ::: $WORD_COUNT "
+  fi
+
+done
