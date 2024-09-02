f7df3995bb04bb4cd6167d8931e7e3ff95203635
RahulYadav22775 <gopichand556677@gmail.com>
2024-09-02 10:21:08 +0530

    assaignments

--- assaignments.sh
+++ assaignments.sh
@@ -17,7 +17,7 @@ echo " word count ::: $COUNT "
 for i in {1..$COUNT}
 do
   word=$(cat $FILE_NAME | awk -F " " '{print $i}')
-  WORD_COUNT=$(cat $FILE_NAME | grep $word | wc -l)
+  WORD_COUNT=$(cat $FILE_NAME | grep "$word" | wc -l)
   echo " word count of $word is :::: $WORD_COUNT "
 
 #   if [ $WORD_COUNT -ne 0 ]
